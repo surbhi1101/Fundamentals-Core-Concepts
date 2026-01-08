a0fbfa005e66eb1c61f1a5c26ec663e0e2057fde
surbhi1101 <surbhij.encircle@gmail.com>
2026-01-08 11:20:50 +0530

    feat:small change done in Assignment-1 of fundamentals core concept

--- assignment-1/src/User.jsx
+++ assignment-1/src/User.jsx
@@ -1,13 +1,8 @@
-import App from './App.jsx'
 function Child(props) {
 
     return (
-        <>
-            <p>{props.details}</p>
-        </>
+        <p>{props.details}</p>
     )
-
-
 }
 
 export default Child
